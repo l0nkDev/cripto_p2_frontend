c76c1716e915b8564ba87f0f516767fe2a6ccb39
l0nkDev <43931415+l0nkDev@users.noreply.github.com>
2025-12-02 20:49:07 -0400

    capacitor ready

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cripto.p2',
+  appName: 'cripto-p2-frontend',
+  webDir: 'dist/cripto_p2_frontend/browser'
+};
+
+export default config;
